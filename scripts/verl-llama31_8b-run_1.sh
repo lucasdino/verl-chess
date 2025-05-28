ff14c17ad5236855fbd046f54c2d5a14f28c6242
Lucas Dionisopoulos <lucasdionisopoulos@gmail.com>
2025-05-28 12:48:07 -0700

    fixed path

--- scripts/verl-llama31_8b-run_1.sh
+++ scripts/verl-llama31_8b-run_1.sh
@@ -1,7 +1,7 @@
 python3 -m verl.trainer.main_ppo \
 algorithm.adv_estimator=gae \
-data.train_files=data/cleaned/verl_tasks/predictmove_verl_500.jsonl \
-data.val_files=data/cleaned/verl_tasks/predictmove_verl_eval_100.jsonl \
+data.train_files=data/cleaned/verl_tasks/train/predictmove_500.parquet \
+data.val_files=data/cleaned/verl_tasks/eval/predictmove_100.parquet \
 data.train_batch_size=8 \
 data.val_batch_size=50 \
 data.max_prompt_length=1000 \
