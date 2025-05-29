536ef6424835947e893b28fb49e0eeecfd099dd7
Lucas Dionisopoulos <lucasdionisopoulos@gmail.com>
2025-05-28 17:59:06 -0700

    updated for debugging

--- verl/utils/reward_score/chess_predictmove.py
+++ verl/utils/reward_score/chess_predictmove.py
@@ -11,22 +11,23 @@ def compute_score(solution_str, ground_truth_str):
         ground_truth (str): The ground truth answer (dict of move to reward -- defined in our dataprocessing code)
     """
     ground_truth = ast.literal_eval(ground_truth_str)    # Need to ast literal eval it since stored as a string due to parquet saving issues
+    
+    # Coerce and catch parse errors
     try:
         predicted_answer = coerce_response(extract_solution(solution_str), task_type="predict_singlemove")
+        if len(predicted_answer) > 10:
+            predicted_answer = None
     except:
-        # Catch for any extraction / coercion errors
         predicted_answer = None
 
-    # Debugging
-    # if False:
-    #     print(f"Extracted answer: {predicted_answer}; In ground truth: {predicted_answer in ground_truth}")
-    #     print(f"Total Generation:\n{solution_str}\n{'='*60}\n\n\n") 
-
-    # Return reward
+    # Giving penalty of -0.2 if not legal move
     if predicted_answer is None:
-        return -0.5
+        reward = -0.5
     else:
-        if predicted_answer in ground_truth:
-            return ground_truth[predicted_answer]
-        else:
-            return -0.2
+        reward = ground_truth.get(predicted_answer, -0.2)
+    
+    # Debugging
+    print(f"Extracted answer: {predicted_answer}; In ground truth: {predicted_answer in ground_truth}; Reward={reward}")
+    # print(f"Total Generation:\n{solution_str}\n{'='*60}\n\n\n") 
+
+    return reward
